4e3ca9c4be0658e7f1d175d729bc7cf74c0a979f
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-10-17 01:39:31 +0200

    render cards dynamically

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,66 +1,39 @@
 import React from "react";
 import "./Card.css";
+import cars from "../CarData";
 
 const Card = () => {
   return (
     <>
-      <div className="card mb-3" style={{ maxWidth: 940 }}>
-        <div className="row g-0">
-          <div className="col-md-4">
-            <img
-              src="https://images.unsplash.com/photo-1672400853593-a57147b50688"
-              className="custom-img-fluid rounded-2"
-              alt="Audi RS3"
-            />
-          </div>
-          <div className="col-md-8">
-            <div className="card-body d-flex flex-column">
-              <h3 className="card-title">Audi RS3</h3>
-              <div className="d-flex gap-4">
-                <h5 className="card-title">Horsepower: 400hp</h5>
-                <h5 className="card-title">0-100km/h: 3.8s</h5>
-              </div>
+      {cars.map(car => (
+        <div className="card mb-3" style={{ maxWidth: 940 }} key={car.model}>
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src={car.image.src}
+                className="custom-img-fluid rounded-2"
+                alt={car.image.alt}
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body d-flex flex-column">
+                <h3 className="card-title">
+                  {car.brand} {car.model}
+                </h3>
+                <div className="d-flex gap-4">
+                  <h5 className="card-title">
+                    Horsepower: {car.horsepower} HP
+                  </h5>
+                  <span>•</span>
+                  <h5 className="card-title">0-100km/h: {car["0-100 km/h"]}</h5>
+                </div>
 
-              <p className="card-text">
-                The Audi RS3 is a high-performance compact car known for its
-                impressive acceleration and handling. The Audi RS3 is a
-                high-performance compact car known for its impressive
-                acceleration and handling. The Audi RS3 is a high-performance
-                compact car known for its impressive acceleration and handling.
-              </p>
+                <p className="card-text">{car.description}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="card mb-3" style={{ maxWidth: 940 }}>
-        <div className="row g-0">
-          <div className="col-md-4">
-            <img
-              src="https://images.unsplash.com/photo-1601278840447-9af5ac4ed157"
-              className="custom-img-fluid rounded-2"
-              alt="Audi RS3"
-            />
-          </div>
-          <div className="col-md-8">
-            <div className="card-body d-flex flex-column">
-              <h3 className="card-title">Audi RS3</h3>
-              <div className="d-flex gap-4">
-                <h5 className="card-title">Horsepower: 400hp</h5>
-                <h5 className="card-title">0-100km/h: 3.8s</h5>
-              </div>
-
-              <p className="card-text">
-                The Audi RS3 is a high-performance compact car known for its
-                impressive acceleration and handling. The Audi RS3 is a
-                high-performance compact car known for its impressive
-                acceleration and handling. The Audi RS3 is a high-performance
-                compact car known for its impressive acceleration and handling.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      ))}
     </>
   );
 };
