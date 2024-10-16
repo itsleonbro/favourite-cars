07a6951ab473be962cf6aa78bf0cdfe4d0307622
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-10-17 01:12:26 +0200

    add components to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Card from "./components/Card/Card";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Navbar />
+      <div className="app-container container mx-md-5">
+        <Card />
+      </div>
+    </>
   );
 }
 
