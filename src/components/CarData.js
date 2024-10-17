d969d34036399b844e15d948641adcc6bf3843ae
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-10-17 02:09:40 +0200

    add car image assets

--- src/components/CarData.js
+++ src/components/CarData.js
@@ -1,74 +1,76 @@
 const cars = [
   {
     image: {
-      src: "https://images.unsplash.com/photo-1672400853593-a57147b50688",
+      src: "./assets/images/audi-rs3.jpeg",
       alt: "Audi RS3",
     },
     brand: "Audi",
     model: "RS3",
-    horsepower: "400 HP",
+    horsepower: "400",
     "0-100 km/h": "4.1 seconds",
     description:
       "The Audi RS3 is a high-performance compact car known for its impressive acceleration and handling.",
   },
   {
     image: {
-      src: "https://images.unsplash.com/photo-1672400853593-a57147b50688",
+      src: "./assets/images/turbo-s.jpg",
       alt: "Porsche 991 Turbo S",
     },
     brand: "Porsche",
     model: "991 Turbo S",
-    horsepower: "640 HP",
+    horsepower: "640",
     "0-100 km/h": "2.7 seconds",
     description:
       "The Porsche 991 Turbo S combines luxury with astonishing performance.",
   },
   {
     image: {
-      src: "https://images.unsplash.com/photo-1672400853593-a57147b50688",
+      src: "./assets/images/audi-rs7.jpg",
       alt: "Audi RS7",
     },
     brand: "Audi",
     model: "RS7",
-    horsepower: "591 HP",
+    horsepower: "591",
     "0-100 km/h": "3.6 seconds",
     description:
       "The Audi RS7 offers a perfect blend of elegance and high performance.",
   },
   {
     image: {
-      src: "",
+      src: "./assets/images/bmw-m4.jpg",
       alt: "BMW M4 Competition",
     },
     brand: "BMW",
     model: "M4 Competition",
-    horsepower: "503 HP",
+    horsepower: "503",
     "0-100 km/h": "3.9 seconds",
     description:
       "The BMW M4 Competition is a sports coupe known for its thrilling driving experience.",
   },
   {
     image: {
-      src: "",
+      src: "./assets/images/dark-horse-2.jpg",
       alt: "Ford Mustang Dark Horse",
     },
     brand: "Ford",
     model: "Mustang Dark Horse",
-    horsepower: "500 HP",
+    horsepower: "500",
     "0-100 km/h": "4.2 seconds",
     description:
       "The Ford Mustang Dark Horse delivers raw power and a modern design.",
   },
   {
     image: {
-      src: "",
+      src: "./assets/images/swift-sport.jpg",
       alt: "Suzuki Swift Sport",
     },
     brand: "Suzuki",
     model: "Swift Sport",
-    horsepower: "138 HP",
+    horsepower: "138",
     "0-100 km/h": "8.1 seconds",
     description:
       "The Suzuki Swift Sport is a compact car known for its agility and fun driving dynamics.",
   },
 ];
+
+export default cars;
